e3c31bde02146053a60e3884b0fd7411729c43dd
popam <popa.mariusz@gmail.com>
2021-08-12 19:54:33 +0200

    nowy plik warsztat2.py

--- Warsztaty.py
+++ Warsztaty.py
@@ -2,8 +2,10 @@
 """
 Wzor na BMI to ...
 """
-'''
-test = '''
-'''
-print(test)
-print('podaj swoja wage :')
+
+waga = float(input('Podaj swoja wage [kg]:'))
+wzrost_cm = float(input('Podaj swoj wzrost [cm]:'))
+wzrost = wzrost_cm / 100.0
+bmi = waga / wzrost**2
+print(f'BMI dla wzrostu {wzrost} i wagi {waga} wynosi {bmi}')
+
