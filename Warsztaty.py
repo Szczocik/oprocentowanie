8918220a840e2b9eaa1e2f778daf061600711263
popam <popa.mariusz@gmail.com>
2021-08-13 23:31:29 +0200

    Wyliczenie sredniej poprawka

--- Warsztaty.py
+++ Warsztaty.py
@@ -7,5 +7,5 @@ waga = float(input('Podaj swoja wage [kg]:'))
 wzrost_cm = float(input('Podaj swoj wzrost [cm]:'))
 wzrost = wzrost_cm / 100.0
 bmi = waga / wzrost**2
-print(f'BMI dla wzrostu {wzrost} i wagi {waga} wynosi {bmi}')
+print(f'BMI dla Twojego wzrostu {wzrost} i wagi {waga} wynosi {bmi}')
 
