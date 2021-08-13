e2ee8d891c66a4a03ef9dacd7a6a6badf6b49caa
popam <popa.mariusz@gmail.com>
2021-08-13 23:26:48 +0200

    Wyliczenie sredniej

---
+++ warsztat2.py
@@ -0,0 +1,13 @@
+
+
+
+
+number1 = float(input('Podaj liczbę 1: '))
+number3 = float(input('Podaj liczbę 3: '))
+number2 = float(input('Podaj liczbę 2: '))
+number4 = float(input('Podaj liczbę 4: '))
+number5 = float(input('Podaj liczbę 5: '))
+number_count = 5
+srednia = (number1 + number2 + number3 + number4 + number5 + number_count)
+print(srednia)
+
